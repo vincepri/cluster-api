5a805a0f58e2a78043c1cbc263f9dbf57da8c323
Vince Prignano <vincepri@vmware.com>
2019-08-12 12:20:41 -0700

    Update vendor dependencies
    Signed-off-by: Vince Prignano <vincepri@vmware.com>

--- vendor/sigs.k8s.io/controller-runtime/pkg/builder/doc.go
+++ vendor/sigs.k8s.io/controller-runtime/pkg/builder/doc.go
@@ -1,5 +1,5 @@
 /*
-Copyright 2016 The Kubernetes Authors.
+Copyright 2018 The Kubernetes Authors.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -14,20 +14,15 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-package generators
+// Package builder provides wraps other controller-runtime libraries and exposes simple
+// patterns for building common Controllers.
+//
+// Projects built with the builder package can trivially be rebased on top of the underlying
+// packages if the project requires more customized behavior in the future.
+package builder
 
 import (
-	"k8s.io/gengo/types"
-	"k8s.io/klog"
+	logf "sigs.k8s.io/controller-runtime/pkg/internal/log"
 )
 
-// extractBoolTagOrDie gets the comment-tags for the key and asserts that, if
-// it exists, the value is boolean.  If the tag did not exist, it returns
-// false.
-func extractBoolTagOrDie(key string, lines []string) bool {
-	val, err := types.ExtractSingleBoolCommentTag("+", key, false, lines)
-	if err != nil {
-		klog.Fatal(err)
-	}
-	return val
-}
+var log = logf.RuntimeLog.WithName("builder")
